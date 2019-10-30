6fa48e2b6c7d4f8c62e2ccfa1ce66f4239717758
EHatem03 <enhat6293@westinghousehs.org>
2019-10-30 22:23:50 +0000

    debugged

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+    $("#button").click(function(){
+        console.log($(".input").val())
+    });
+
+console.log("hello")
 
 
-  
-  
-  
-  
 });
+console.log('script');
