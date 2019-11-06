4304af6fc431ab546af3c342487109ba7b0cae55
EHatem03 <enhat6293@westinghousehs.org>
2019-11-06 21:43:39 +0000

    Declared variable

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
-    $("#button").click(function(){
-        console.log($(".input").val())
-    });
 
-console.log("hello")
+$("#button").click(function() {
+   let message=
+   $("input").val();
+
+});
+
+    newFunction_1();
 
 
 });
 console.log('script');
+
+function newFunction_1() {
+    newFunction();
+}
+
+function newFunction() {
+    console.log("hello");
+}
+
